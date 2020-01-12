3c93d89440af43948afd1549d172070818432cb3
gcm <gcaptainmarvel@gmail.com>
2020-01-12 23:58:17 +0200

    IOASA 0.9

--- ioasa/settings.py
+++ ioasa/settings.py
@@ -11,9 +11,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '9#89+c1953pu^v8r-=2#v1mmgh&lfckzea^3dl8&r#!k40ra6t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ioasa.org']
 
 
 # Application definition
