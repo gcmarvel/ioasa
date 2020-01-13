2eaad7c227a42eec0e659f0e05217c939852b57c
gcm <gcaptainmarvel@gmail.com>
2020-01-13 16:23:56 +0200

    IOASA 0.9

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,27 @@
+// Read More js
+
+let content = document.querySelectorAll('.article-content'),
+    expand = document.querySelectorAll('.button-expand'),
+    collapse = document.querySelectorAll('.button-collapse');
+
+expand[0].addEventListener('click', function() {readMore(0)});
+expand[1].addEventListener('click', function() {readMore(1)});
+
+collapse[0].addEventListener('click', function() {readLess(0)});
+collapse[1].addEventListener('click', function() {readLess(1)});
+
+function readMore(i) {
+    content[i].style.maxHeight = 'inherit';
+    expand[i].style.display = 'none';
+    collapse[i].style.display = 'inline';
+}
+
+function readLess(i) {
+    content[i].style.maxHeight = '50vw';
+    expand[i].style.display = 'inline';
+    collapse[i].style.display = 'none';
+}
+
 //Catalogue js
 
 const tableRows = document.querySelectorAll('.table-row');
